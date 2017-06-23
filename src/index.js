724f31546243f491a2c923eb92821963caade88f
mrc535s <carney535@yahoo.com>
2017-06-23 10:45:43 -0500

    render multiple

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {AutoComplete} from './components/auto-complete'
+import {AutoComplete} from './components/auto-complete';
 
-ReactDOM.render(
-  <AutoComplete/>,
-  document.getElementById('root')
-);
+(function() {
+	document.addEventListener("DOMContentLoaded", function(event) {
+		let elements = document.getElementsByClassName("root");
+		for (let i=0; i<=elements.length; i++) {
+			//createAutoCompleteComponent(elements[i]);
+		}
+	});
+
+	createAutoCompleteComponent(document.getElementById("root"));
+	createAutoCompleteComponent(document.getElementById("root2"));
+	function createAutoCompleteComponent(element) {
+		ReactDOM.render(
+			<AutoComplete/>,
+			element
+		);
+	}
+
+	window.createAutoCompleteComponent = createAutoCompleteComponent;
+}())
