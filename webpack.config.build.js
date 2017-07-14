be35bd215dce8fb3e5fce74e4281ea9d94f3afc7
mrc535s <carney535@yahoo.com>
2017-07-13 21:18:12 -0500

    separated webpack config into separate files

---
+++ webpack.config.build.js
@@ -0,0 +1,15 @@
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+   }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 const path = require('path');
-const examples = process.argv[2] === 'examples' ? true : false;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = examples ? new HtmlWebpackPlugin({
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './examples/index.html',
   filename: 'index.html',
   inject: 'body'
-}) : null;
-const plugins = HtmlWebpackPluginConfig ? [HtmlWebpackPluginConfig] : []
+});
 module.exports = {
-  entry: examples ? './examples/index.js' : './src/index.js',
+  entry: './examples/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index.js',
-    libraryTarget: examples ? 'var' : 'commonjs2'
+    filename: 'index.js'
   },
   module: {
     loaders: [
@@ -20,5 +17,5 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
    },
-	 plugins: plugins
+	 plugins: [HtmlWebpackPluginConfig]
 }
